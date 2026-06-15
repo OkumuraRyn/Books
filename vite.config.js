81a3b5558aafc7ed988d417127c5bc0c6a953727
OkumuraRyn <166512321+OkumuraRyn@users.noreply.github.com>
2026-06-15 18:52:49 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-plugin-svg-loader'
+
+export default defineConfig({
+  plugins: [vue(), svgLoader()],
+  server: {
+    proxy: {
+      '/api/deepseek': {
+        target: 'https://api.deepseek.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/deepseek/, '')
+      }
+    }
+  }
+})
