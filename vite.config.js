84f090e229fdfc17e952ace321bc413071c9a524
OkumuraRyn <166512321+OkumuraRyn@users.noreply.github.com>
2026-06-17 09:45:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-plugin-svg-loader'
 
 export default defineConfig({
-  base: 'Books',  // 替换为你的仓库名
-  plugins: [vue(), svgLoader()]
+  base: '/Books/',
+  plugins: [vue()]
 })
